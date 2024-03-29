726b7e66fa2e0b0edd60ab93b1185fb1d48e0797
Irfaan Meera.E <irfaanmeera@gmail.com>
2024-03-29 18:20:38 +0530

    smtp changed

--- services/sendOtp.js
+++ services/sendOtp.js
@@ -1,10 +1,7 @@
 const nodemailer = require('nodemailer')
 
    const transporter= nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port:587,
-      secure:false,
-      requireTLS:true,
+      service:'gmail',
       auth:{
           user:'irfaanmeera@gmail.com',
           pass:'kjwl efmz fvhy mvbp'
