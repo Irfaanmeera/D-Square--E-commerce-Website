cf91bc8d63aba750c4d06702d458864da54b1bcf
Irfaan Meera.E <irfaanmeera@gmail.com>
2024-03-30 12:46:52 +0530

    nodemailer changes

--- services/sendOtp.js
+++ services/sendOtp.js
@@ -1,16 +1,35 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config(); // Load environment variables from .env file
 
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: true,
+//     auth: {
+//         user: process.env.GMAIL_ID,
+//         pass: process.env.GMAIL_PASSWORD,
+//         method: 'PLAIN' // Ensure the authentication method is specified
+//     }
+// });
+
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
+    service:'gmail',
+    port: 465,
     secure: true,
+    logger:true,
+    debug:true,
+    secureConnection:false,
     auth: {
         user: process.env.GMAIL_ID,
         pass: process.env.GMAIL_PASSWORD,
-        method: 'PLAIN' // Ensure the authentication method is specified
+        
+    },
+    tls:{
+      rejectUnAuthorised : true,
     }
 });
+
+
 
 // Rest of your email sending logic...
 module.exports = transporter;
